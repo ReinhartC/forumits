df56c0ac5f3aabfe05cbed69d7b2c38ee6106f21
Reinhart Caesar <sgs3.rc@gmail.com>
2018-05-24 18:30:52 +0700

    versi

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
   <div class="container">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 1.0
+      <b>Version</b> 1.1
     </div>
     <strong>Copyright &copy; 2018</strong> FITS Dev Team
   </div><br>
